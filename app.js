343d1c15fa63317adc4e6ada716bb6ab6c5fc97b
FernandoVini <72674119+FernandoVini@users.noreply.github.com>
2021-05-30 18:21:00 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('./public'));
+
+app.get('/', function(req,res){
+    res.render('home/index.ejs');
+});
+
+app.get('/Var', function(req,res){
+    res.render('Var/Variavel_Ex');
+});
+
+app.get('/Let', function(req,res){
+    res.render('Let/Let_Ex');
+});
+
+app.listen(3000, function(){
+    console.log("Servidor rodando com Express");
+});
